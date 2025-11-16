ca305c0decc9c4f5e4f3355b3de4ddb1a71ff765
Mark Young <markyoung0110@gmail.com>
2025-11-16 16:13:38 -0500

    completed working program

--- watermelon.py
+++ watermelon.py
@@ -1,10 +1,11 @@
 weight = int(input("Enter the weight of the watermelon: "))
 if weight %2 != 0 or weight == 2:
-    print("Cannot be divided into two even parts")
+    print("NO")
 else:
     pairs = []
-    for first_part in range(2, weight//2 + 1, 2):
+    for first_part in range(2, weight//2 + 1, 2): #this ensures first_part is even, gets the weight divided by 2, and only checks even numbers
         second_part = weight - first_part
-        if second_part % 2 == 0:
+        if second_part % 2 == 0: # this checks to see if the second part is even
             pairs.append((first_part, second_part))
-    print(pairs)
+    print("YES")        
+    
